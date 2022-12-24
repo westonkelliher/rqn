ae03b2dd76b47130f9f71e3a74ce459f4aebd9d5
Weston Kelliher <westonkelliher@gmail.com>
2022-12-24 12:17:33 -0800

    forgot to update WebCP to the websocket branch

--- webcp/index.js
+++ webcp/index.js
@@ -2,48 +2,21 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const Server = require("socket.io");
-const io = new Server(server);
-const net = require('net');
 
-
+var x = 0;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', (websock) => {
-    console.log('a user connection');
-    var rawsock = net.connect({port:50079}, function() {
-	console.log('cp_sock connected to cp_server');
-    });
-    rawsock.on('error', function(data) {
-	console.log("cp_sock closed by error");
-    });
-
-    var buffer = "";
-    rawsock.on('data', function(data) {
-	console.log("cp_sock got " + data.toString());
-	buffer += data;
-	const parts = buffer.split('\n');
-	if (parts.length > 1) {
-	    buffer = parts[parts.length - 1];
-	    const messages = parts.slice(0, parts.length - 1);
-	    for (let m of messages) {
-		websock.emit('msg', m.toString());
-	    }
-	}
-    });
-    
-    websock.on('disconnect', () => {
-	rawsock.destroy();
-	console.log('user disconnected');
-    });
-    websock.on('msg', (msg) => {
-	//console.log('msg', msg);
-	console.log("from browser :" + msg);
-	rawsock.write(msg + '\n');
-    });
+    /*console.log(x);
+    if (x == 0) {
+	res.sendFile(__dirname + '/test.html');
+	x = 1;
+    } else {
+	res.sendFile(__dirname + '/index.html');
+	x = 0;
+    }
+    console.log(x);
+    console.log("-");*/
 });
 
 
