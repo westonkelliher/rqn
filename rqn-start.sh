e065e6fcfa16c14a0b128271e77b9b2af851e945
Weston Kelliher <westonkelliher@gmail.com>
2023-01-22 08:43:08 -0800

    testing new loader (just seeing if we access graphical)

--- rqn-start.sh
+++ rqn-start.sh
@@ -10,19 +10,10 @@ echo "....................."
 log="/home/requin/logs"
 dest="/home/requin/rqn"
 
-# run loader
-echo "python3 $dest/loader.py"
+# run recbox system applications (AppSwitcher)
+echo "$dest/loader"
 echo "$(date)" > $log/loader.out
-python3 $dest/loader.py 1>> $log/loader.out 2>> $log/loader.out
-
-# OTA update
-echo "$dest/ota.sh"
-echo "$(date)" > $log/ota.out
-$dest/ota.sh 1>> $log/ota.out 2>> $log/ota.out
-
-# placeholder in case we need to run something here without changing ota.sh
-echo "$dest/configure.sh"
-$dest/configure.sh 1> $log/configure.out 2> $log/configure.out
+$dest/loader 1> $log/loader.out 2> $log/loader.out &
 
 # kill any existing control pad servers
 pid=$(ps aux | grep cp_server | grep -v "grep" | sed 's/^[^[:space:]]\+[[:space:]]\+\([0-9]\+\).*/\1/')
@@ -37,8 +28,3 @@ $dest/cp_server 1> $log/cp_server.out 2> $log/cp_server.out &
 # run web server
 echo "node $dest/webcp/index.js"
 node $dest/webcp/index.js 1> $log/node.out 2> $log/node.out &
-
-# run game launcher
-echo "$dest/launcher"
-echo "$(date)" > $log/launcher.out
-$dest/launcher 1>> $log/launcher.out 2>> $log/launcher.out
