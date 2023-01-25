8471171754da8c121f69ae53ce910b60f02faec4
Weston Kelliher <westonkelliher@gmail.com>
2023-01-25 13:30:26 -0800

    have loader launch other things

--- rqn-start.sh
+++ rqn-start.sh
@@ -10,23 +10,15 @@ echo "....................."
 log="/home/requin/logs"
 dest="/home/requin/rqn"
 
-# run recbox system applications (AppSwitcher)
-echo "$dest/loader"
-echo "$(date)" > $log/loader.out
-$dest/loader 1> $log/loader.out 2> $log/loader.out
-
-sleep infinity
-
 # kill any existing control pad servers
 pid=$(ps aux | grep cp_server | grep -v "grep" | sed 's/^[^[:space:]]\+[[:space:]]\+\([0-9]\+\).*/\1/')
 for x in $pid; do
     kill -9 $x
 done
 
-# run control pad server
-echo "$dest/cp_server"
-$dest/cp_server 1> $log/cp_server.out 2> $log/cp_server.out &
+# run recbox system applications (Loader which runs Launcher)
+echo "$dest/loader"
+echo "$(date)" > $log/loader.out
+$dest/loader 1> $log/loader.out 2> $log/loader.out
 
-# run web server
-echo "node $dest/webcp/index.js"
-node $dest/webcp/index.js 1> $log/node.out 2> $log/node.out &
+sleep infinity
